dcab462cb606c9f83b0fa0a05bea6edc5670b48f
Samuel Song <samuel.song@sap.com>
2019-05-24 18:20:07 +0800

    Add print_in_visible_whitespace function

--- text-to-html/text_to_html.pl
+++ text-to-html/text_to_html.pl
@@ -1,10 +1,19 @@
 #!/usr/bin/perl
 undef $/;
+
+sub print_in_visible_whitespace {
+  my $string = $_[0];
+  $string =~ s/ /·/g;
+  $string =~ s/\n/¬\n/g;
+  print "$string\n";
+}
+
 $text = <>;
-$text =~ s/(\n)(\s*)(?=\n)/$1<p>/g;
 
-# make whitespace visible
-$text =~ s/ /·/g;
-$text =~ s/\n/¬\n/g;
+print "Input:\n";
+print_in_visible_whitespace($text);
 
-print "$text\n";
+$text =~ s/(\n)(\s*)(\n)/$1<p>$3/g;
+
+print "Output:\n";
+print_in_visible_whitespace($text);
